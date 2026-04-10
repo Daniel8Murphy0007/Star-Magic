13e161675cf8822c1a6722f7c270250c25bcc67b
Daniel T Murphy <daniel.murphy00@gmail.com>
2026-04-09 20:04:07 -0400

    Session 204: 10 advanced physics frameworks added to CP2 (680 classes)
    TQFT anyon braiding + Chern-Simons, Swampland conjectures (WGC/dS/distance),
    Higher-Curvature gravity (Gauss-Bonnet/f(R)/Horndeski), SUSY breaking soft terms,
    cMERA entanglement RG (holographic), Quantum Error Correction topo codes,
    Non-Commutative Geometry matrix models, Loop Quantum Gravity (Ashtekar/area spectrum),
    Color Glass Condensate (BK/saturation), Resummation effective coupling (BFKL/Sudakov).
    
    All 10 classes: syntax OK, AST verified, compute() tested.
    CP2: 50,855 lines, 680 classes (was 668).

--- CondensedPhysics2.py
+++ CondensedPhysics2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5021aaa0d05f0bc9651cb047ade872594dd419d99383d1cac0ffb09f0e0363e
-size 1947930
+oid sha256:1b55364dfe8dba964250f578f97a6dcd59dec9153745a636d0e53fa12df0a2f0
+size 1970755
