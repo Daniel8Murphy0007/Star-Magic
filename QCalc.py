b3cdb52bcd8ef52dab5735027cd7f87a9a56cd0f
Daniel T Murphy <daniel.murphy00@gmail.com>
2026-04-17 00:54:10 -0400

    DPM-emergent fix: QCalc.py quadratic gravity g_newtonian -> g_dpm

--- QCalc.py
+++ QCalc.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32b7c189cc2276a29e5cf9242d5af324a34304ca8edbcc4f546cadf5cb27fa33
-size 465517
+oid sha256:ffd726538db8a2adee485e15f34745bd90fc725edec8e71b361011164065e4c6
+size 465627
