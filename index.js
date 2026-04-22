a8e0aafd6368e9a176028f5f9607841b6db6f45d
Daniel T Murphy <daniel.murphy00@gmail.com>
2026-04-22 17:26:20 -0400

    Revert r=0 guard in calculateDipMomentumEnergy (UQFF physics must determine r context)

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f86b15b67dfe05ccd93d9b08a2e85cdc89bc10eb21e7f11a077943480d30de55
-size 1167088
+oid sha256:592a5c2a71e5c12b74f4df64c21e6891003ef3c5521fba1b871e0708d2c944e6
+size 1167007
