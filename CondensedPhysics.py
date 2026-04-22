7502b2a7a115316fff7731d5b4dd7d1a96196c60
Daniel T Murphy <daniel.murphy00@gmail.com>
2026-04-22 11:24:53 -0400

    Strip 686K excess blank lines from CP1: 891637 -> 205034 lines (77% reduction); all blank lines were injected by DPM-emergent audit scripts; syntax OK; 160929 non-blank lines preserved

--- CondensedPhysics.py
+++ CondensedPhysics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0da53d6a0e9aa91bc4b68c212a6ec0cd93c798c8c67456a19337b194a250ebb
-size 9885958
+oid sha256:d5dfdc27ee8214ef666b262e954b9ca26264036e53839fd1a4c6514f20abb93b
+size 8137403
