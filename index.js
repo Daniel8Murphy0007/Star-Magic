c386e788c012a35c150b8beef2e893d37129e1db
Daniel T Murphy <daniel.murphy00@gmail.com>
2026-04-26 11:49:27 -0400

    fix index.js: parkhomovExcessHeat N_clusters=1e22->2e18, 630 eV/cluster formula (not E_PHONON*S26_3*PHI product); same fix to mizunoLENRExcessHeat; delete _merge_99system.py temp script

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21a5a0fabab3fa617df4692e1784260e71bfa2b0dbbe5ece964295049210ada1
-size 1198356
+oid sha256:d3be8dd1150fd5d200db11b606843466e0c92fd6210575794324de874753bca5
+size 1198445
