b8bf158a01e5ee16651ea31d78635c285be59dbd
Daniel T Murphy <daniel.murphy00@gmail.com>
2026-04-27 09:18:52 -0400

    fix: add missing dpm_ug1_seed import to QCalc.py (NameError at 13 call sites)

--- QCalc.py
+++ QCalc.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0794433196eec1efb0f121a91e99a8c76809fb35525a897bad71f85240320e66
-size 467606
+oid sha256:e423ebc0ef64668c135c6ed4e35df11fc70b0d0efd6547c1d51fddb7e6c0328e
+size 467717
