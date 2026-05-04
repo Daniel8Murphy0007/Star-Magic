d97540a1a7c081e061e5f416aa04e14576ab897b
Daniel T Murphy <daniel.murphy00@gmail.com>
2026-05-04 17:15:19 -0400

    Fix AGNFeedbackFactorCalculator + AetherCouplingCalculator: guard against dataset dict passed as first arg

--- CondensedPhysics2.py
+++ CondensedPhysics2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34b0cded2670e5a354c11bdb8726274933d9c44143baadf2bf8b0eb8889d9c1f
-size 2233852
+oid sha256:50bb7210146bcdf84d7efe7a18a53372b49a5bca44c114752eb0fca7fb5d1cb9
+size 2234159
