a316cb80c771b8d0b4949f31524b4a3c5941c2eb
Star-Magic Dev <dev@star-magic.local>
2026-06-26 23:20:55 -0400

    Round 23 forensic recovery: a009fadc surgical mojibake fix in CP2 SCm-class range

--- CondensedPhysics2.py
+++ CondensedPhysics2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:437ddfafded96a56818786e580b5e3c022a4448ea32392a39d91f6b342aecdf2
-size 2252601
+oid sha256:199abcc65e4a6e13583e66d92488308663a8ce7b2f742412346c8e20a9de8d38
+size 2252546
