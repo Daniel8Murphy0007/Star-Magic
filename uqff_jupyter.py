dadf133cff60086853d5f92f0031bc73c052ebb0
Daniel T. Murphy <daniel.murphy00@enrgyone.com>
2026-07-17 13:13:09 -0400

    v5.69.1: fix pyproject description 602->424 chars (PyPI 512 limit); _VERSION bump to 5.69.1 in 3 py-modules

--- uqff_jupyter.py
+++ uqff_jupyter.py
@@ -41,7 +41,7 @@ except ImportError:
     def _all_bucket_observables(): return {}
 
 
-_VERSION = "5.69.0"
+_VERSION = "5.69.1"
 
 
 # ---------------------------------------------------------------------------
